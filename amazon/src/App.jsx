2bb2dd58459829ced53abf93d7af0b9b92075f84
SindhuR7 <alirazasindhu066@gmail.com>
2025-09-30 21:47:45 +0500

    App

--- amazon/src/App.jsx
+++ amazon/src/App.jsx
@@ -1,11 +1,20 @@
 import SignIn from "./pages/SignIn"
+import{ BrowserRouter, Route, Routes} from 'react-router-dom'
+import SignUp from "./pages/SignUp"
+import VerifyEmail from "./pages/VerifyEmail"
 
 
 function App() {
 
   return (
     <>
-    <SignIn/>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<SignIn/>}/>
+      <Route path="/signup" element={<SignUp/>}/>
+      <Route path="/verify" element={<VerifyEmail/>} />
+    </Routes>
+    </BrowserRouter>
     </>
   )
 }
